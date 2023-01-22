045adc12479627be6950efb6ac303784f70ad741
Ari Meisels <me@arimeisels.com>
2023-01-22 13:24:20 -0500

    Add computer vision for 20 points yayay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -7,6 +7,14 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+import java.util.ArrayList;
+
 @Autonomous(name="Autonomus", group="Linear Opmode")
 public class AutoOpMode extends LinearOpMode {
     private MecanumWheels drive;
@@ -21,15 +29,15 @@ public class AutoOpMode extends LinearOpMode {
     private CRServo armMotor1;
     private CRServo armMotor2;
 
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-        drive = new MecanumWheels(leftBackMotor, leftFrontMotor, rightBackMotor, rightFrontMotor);
-        arm = new Arm(armServo, armMotor1, armMotor2);
 
         leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
         leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
@@ -40,10 +48,56 @@ public class AutoOpMode extends LinearOpMode {
         armMotor1 = hardwareMap.get(CRServo.class, "armMotor1");
         armMotor2 = hardwareMap.get(CRServo.class, "armMotor2");
 
+        drive = new MecanumWheels(leftBackMotor, leftFrontMotor, rightBackMotor, rightFrontMotor);
+        arm = new Arm(armServo, armMotor1, armMotor2);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(0.166, 580, 580, 400, 220);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) { }
+        });
+
         waitForStart();
         runtime.reset();
 
         while (opModeIsActive()) {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == 0) {
+                        runtime.reset();
+                        drive.drive(-1, 0, -1, 0);
+                        while (runtime.milliseconds() < 1000) { }
+                        drive.drive(0, -1, 0, -1);
+                        while (runtime.milliseconds() < 2000) { }
+                        drive.drive(0, 0, 0, 0);
+                    } else if (tag.id == 19) {
+                        runtime.reset();
+                        drive.drive(0, -1, 0, -1);
+                        while (runtime.milliseconds() < 750) { }
+                        drive.drive(0, 0, 0, 0);
+                    } else if (tag.id == 242) {
+                        runtime.reset();
+                        drive.drive(1, 0, 1, 0);
+                        while (runtime.milliseconds() < 1000) { }
+                        drive.drive(0, -1, 0, -1);
+                        while (runtime.milliseconds() < 2000) { }
+                        drive.drive(0, 0, 0, 0);
+                    }
+                    stop();
+                }
+            }
+
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
         }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MayTags.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+import java.util.ArrayList;
+
+@Autonomous(name="MayTags bc April fake", group="Linear Opmode")
+public class MayTags extends LinearOpMode {
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    double tagSize = 0.166;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagSize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) { }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == 0) {
+                        telemetry.addData("TAG", "left");
+                    } else if (tag.id == 19) {
+                        telemetry.addData("TAG", "forward");
+                    } else if (tag.id == 242) {
+                        telemetry.addData("TAG", "right");
+                    }
+                    telemetry.update();
+                }
+            }
+        }
+    }
+}
