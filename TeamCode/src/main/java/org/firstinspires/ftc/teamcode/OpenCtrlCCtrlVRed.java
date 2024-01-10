a6e1074bb3950c96654dd339daa964b8dc36a9b8
Ima Person <me@imaperson.dev>
2024-01-10 17:55:08 -0500

    Swap AprilTags with OpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCtrlCCtrlVRed.java
@@ -0,0 +1,47 @@
+// https://gist.github.com/oakrc/12a7b5223df0cb55d7c1288ce96a6ab7
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@TeleOp(name="OpenCtrlCCtrlV (Red)", group="Object Detection")
+public class OpenCtrlCCtrlVRed extends LinearOpMode {
+    OpenCvCamera camera;
+    PropDetectionPipeline propDetectionPipeline;
+
+    ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        propDetectionPipeline = new PropDetectionPipeline(true);
+
+        camera.setPipeline(propDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() { camera.startStreaming(1280, 720, OpenCvCameraRotation.SENSOR_NATIVE); }
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Position of biggest red blob", new String[]{ "None", "Left", "Middle", "Right" }[propDetectionPipeline.getBiggestBlob() + 1]);
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PropDetectionPipeline.java
@@ -0,0 +1,98 @@
+// https://www.youtube.com/watch?v=rWp76RV_Cik bad
+// https://youtu.be/0_w7UTN9LnE
+
+package org.firstinspires.ftc.teamcode;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.*;
+
+public class PropDetectionPipeline extends OpenCvPipeline {
+    boolean checkRed;
+    private int biggestBlob = -1;
+
+    public PropDetectionPipeline(boolean checkRed) { this.checkRed = checkRed; }
+
+    public int getBiggestBlob() { return biggestBlob; }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Rect left = new Rect(1, 1, (1280 / 3) - 1, 719);
+        Rect middle = new Rect(1280 / 3, 1, (1280 / 3) - 1, 719);
+        Rect right = new Rect((1280 / 3) * 2, 1, (1280 / 3) - 1, 719);
+        Imgproc.rectangle(input, left, new Scalar(0, 255, 0), 2);
+        Imgproc.rectangle(input, middle, new Scalar(0, 255, 0), 2);
+        Imgproc.rectangle(input, right, new Scalar(0, 255, 0), 2);
+
+        Mat mask = preprocessFrame(input);
+
+        Mat[] segregatedMats = { mask.submat(left), mask.submat(middle), mask.submat(right) };
+        Mat[] segregatedMatsColor = { input.submat(left), input.submat(middle), input.submat(right) };
+        double[] areas = new double[3];
+
+        for (int i = 0; i < segregatedMats.length; i++) {
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(segregatedMats[i], contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            hierarchy.release();
+
+            MatOfPoint largestContour = findLargestContour(contours);
+            if (largestContour != null) {
+                Imgproc.drawContours(segregatedMatsColor[i], contours, -1, checkRed ? new Scalar(0, 0, 155) : new Scalar(155, 0, 0), 5);
+                Imgproc.drawContours(segregatedMatsColor[i], contours, contours.indexOf(largestContour), checkRed ? new Scalar(0, 0, 255) : new Scalar(255, 0, 0), 5);
+                areas[i] = Imgproc.boundingRect(largestContour).area();
+            } else areas[i] = 0;
+            segregatedMats[i].release();
+        }
+        Core.hconcat(Arrays.asList(segregatedMatsColor), input);
+        for (Mat mat : segregatedMatsColor) mat.release();
+        mask.release();
+
+        double greatest = Math.max(areas[0], (Math.max(areas[1], areas[2])));
+        if (greatest == 0) biggestBlob = -1;
+        else if (greatest == areas[0]) biggestBlob = 0;
+        else if (greatest == areas[1]) biggestBlob = 1;
+        else if (greatest == areas[2]) biggestBlob = 2;
+
+        return input;
+    }
+
+    private Mat preprocessFrame(Mat frame) {
+        Mat hsvFrame = new Mat();
+        Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_RGB2HSV);
+
+        Mat mask = new Mat();
+        if (checkRed) {
+            Mat lowerRed = new Mat(), upperRed = new Mat();
+            Core.inRange(hsvFrame, new Scalar(0, 50, 50), new Scalar(10, 255, 255), lowerRed);
+            Core.inRange(hsvFrame, new Scalar(170, 50, 50), new Scalar(180, 255, 255), upperRed);
+            Core.bitwise_or(lowerRed, upperRed, mask);
+            lowerRed.release();
+            upperRed.release();
+        } else Core.inRange(hsvFrame, new Scalar(100, 150, 0), new Scalar(140, 255, 255), mask);
+        hsvFrame.release();
+
+        return mask;
+    }
+
+    private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
+        double maxArea = 0;
+        MatOfPoint largestContour = null;
+
+        for (MatOfPoint contour : contours) {
+            double area = Imgproc.contourArea(contour);
+            if (area > maxArea) {
+                maxArea = area;
+                largestContour = contour;
+            }
+        }
+
+        return largestContour;
+    }
+}
